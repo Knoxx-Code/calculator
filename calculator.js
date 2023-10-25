9bbbfabcc91b13b525d95b7d0de73790fa78a607
Knoxx <37295154+Winston-Churchill@users.noreply.github.com>
2023-10-25 12:14:10 +0300

    Created functions to handle the math operations

---
+++ calculator.js
@@ -0,0 +1,26 @@
+
+//Addition
+function add(a,b){
+    return a + b;
+}
+
+//Subtraction
+function subtract(a,b){
+    return a - b;
+}
+
+//Multiplication
+function multiply(a,b){
+    return a * b;
+}
+
+//Division
+function divide(a,b){
+    return a / b;
+}
+
+//Modulus operation
+function modulus(a,b){
+    return a % b ;
+}
+
